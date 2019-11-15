c0475972d90a5fb92f7ae5695e35b296445e9b1d
Ja-Eun Yu <yujaaa0219@gmail.com>
2019-11-14 22:42:27 -0600

    Add files via upload

---
+++ controller/sentence_controller.js
@@ -0,0 +1,34 @@
+function getSentences(word){
+    let sentenceArr= [];
+    let wordArr=[];
+    let metaphorArr=[];
+    for (let d in linkArray){
+        //console.log(linkArray[d])
+        if(linkArray[d].source == word || linkArray[d].target == word){
+            sentenceArr.push(linkArray[d].sentence)
+            wordArr.push(linkArray[d].source)
+            metaphorArr.push(linkArray[d].target)
+        }
+    }
+
+    document.getElementById('sentences-list').innerHTML = ''
+
+    //print 
+    for(let d in sentenceArr){
+
+        console.log(sentenceArr[d]);
+        var newNode = document.createElement('p');
+        console.log(wordArr[d]);
+        //highlight words
+        sentenceArr[d] = sentenceArr[d].replace(wordArr[d], "<text style='color:limegreen; font-size:18px'>"+wordArr[d]+"</text>");
+        sentenceArr[d] = sentenceArr[d].replace(metaphorArr[d], "<text style='color:orange; font-size:18px'>"+metaphorArr[d]+"</text>");
+        console.log(sentenceArr[d]);
+        newNode.innerHTML="* " + sentenceArr[d];
+
+        // Add the contents of sentences to #sentences-tab
+        document.getElementById('sentences-list').appendChild(newNode);
+    }
+
+
+}
+
