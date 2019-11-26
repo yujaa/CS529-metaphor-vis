6eb061331d123daf3fef816796f94ae6edb5af3b
Ja-Eun Yu <yujaaa0219@gmail.com>
2019-11-26 10:16:29 -0600

    Add POS filter

--- controller/filter_controller.js
+++ controller/filter_controller.js
@@ -2,16 +2,38 @@ var App = App ||{};
 
 var filteredGraph;
 
+var keyword;
+var checked = ["verb"];
 
 function search(){
   d3.json("./model/data/train_graph_data.json", function(error, data) {
-    var keyword = document.getElementById('search-box').value;
+    keyword = document.getElementById('search-box').value;
     filter_by_keyword(data,keyword);
 
   })
   
 }
 
-function histogram_brushing(){
+function checkbox(){
 
+  App.checked = [];
+  if(document.getElementById("noun").checked)
+    checked.push("Noun");
+  if(document.getElementById("pronoun").checked)
+    checked.push("Pronoun");
+  if(document.getElementById("verb").checked)
+    checked.push("Verb");
+  if(document.getElementById("adjective").checked)
+    checked.push("Adjective");
+  if(document.getElementById("adverb").checked)
+    checked.push("Adverb");
+
+
+  filter_by_keyword(App.data,App.keyword);
+  return checked;
+}
+
+function getChecked()
+{
+  return checked;
 }
