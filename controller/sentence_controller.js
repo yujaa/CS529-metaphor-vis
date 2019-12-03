c01b6b97cbf0c6fd81499185fe822da09966c6ba
Anusha Sagi <anushaaug21@gmail.com>
2019-12-03 10:33:10 -0600

    Add files via upload

--- controller/sentence_controller.js
+++ controller/sentence_controller.js
@@ -12,6 +12,9 @@ function getSentences(word){
     }
 
     document.getElementById('sentences-list').innerHTML = ''
+    var node_ol = document.createElement('p');
+    node_ol.innerHTML="<ol> ";
+    document.getElementById('sentences-list').appendChild(node_ol);
 
     //print 
     for(let d in sentenceArr){
@@ -20,14 +23,17 @@ function getSentences(word){
         var newNode = document.createElement('p');
         console.log(wordArr[d]);
         //highlight words
-        sentenceArr[d] = sentenceArr[d].replace(" "+wordArr[d], "<text style='color:white; background-color: limegreen; font-size:14px'>"+" "+wordArr[d]+"</text>");
-        sentenceArr[d] = sentenceArr[d].replace(" "+metaphorArr[d], "<text style='color:white; background-color: orange; font-size:14px'>"+" "+metaphorArr[d]+"</text>");
+        sentenceArr[d] = sentenceArr[d].replace(" "+wordArr[d], " "+"<text style='color:white; background-color: limegreen; font-size:14px'>"+""+wordArr[d]+"</text>");
+        sentenceArr[d] = sentenceArr[d].replace(" "+metaphorArr[d], " "+"<text style='color:white; background-color: orange; font-size:14px'>"+""+metaphorArr[d]+"</text>");
         console.log(sentenceArr[d]);
-        newNode.innerHTML="* " + sentenceArr[d];
+        newNode.innerHTML="<li> " + sentenceArr[d] + "</li>";
 
         // Add the contents of sentences to #sentences-tab
         document.getElementById('sentences-list').appendChild(newNode);
     }
+
+    node_ol.innerHTML= " </ol>";
+    document.getElementById('sentences-list').appendChild(node_ol);
 
 
 }
