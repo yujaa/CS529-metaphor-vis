afd487d98dee2753755ef2bebd7cf3e20f52e599
Anusha Sagi <anushaaug21@gmail.com>
2019-12-03 11:09:23 -0600

    Modify POS tags

--- controller/sentence_controller.js
+++ controller/sentence_controller.js
@@ -23,12 +23,47 @@ function getSentences(word){
     //print 
     for(let d in sentenceArr){
 
+        wordPOS_color = ""
+        metaphorPOS_color = ""
         console.log(sentenceArr[d]);
         var newNode = document.createElement('p');
         console.log(wordArr[d]);
+        //Adding color to POS Tags
+        if (wordPos_Arr[d] == "Noun")
+        {
+            wordPOS_color = "<text style='color:white; background-color: red; font-size:14px'>"+ "(" + wordPos_Arr[d] + ")" + "</text>" 
+        }
+        else if (wordPos_Arr[d] == "Verb")
+        {
+            wordPOS_color = "<text style='color:white; background-color: blue; font-size:14px'>"+ "(" + wordPos_Arr[d] + ")" + "</text>" 
+        }
+        else if (wordPos_Arr[d] == "Adjective")
+        {
+            wordPOS_color = "<text style='color:white; background-color: yellow; font-size:14px'>"+ "(" + wordPos_Arr[d] + ")" + "</text>" 
+        }
+        if (wordPos_Arr[d] == "Adverb")
+        {
+            wordPOS_color = "<text style='color:white; background-color: green; font-size:14px'>"+ "(" + wordPos_Arr[d] + ")" + "</text>" 
+        }
+        if (metaphorPos_Arr[d] == "Noun")
+        {
+            metaphorPOS_color = "<text style='color:white; background-color: red; font-size:14px'>"+ "(" + metaphorPos_Arr[d] + ")" + "</text>" 
+        }
+        else if (metaphorPos_Arr[d] == "Verb")
+        {
+            metaphorPOS_color = "<text style='color:white; background-color: blue; font-size:14px'>"+ "(" + metaphorPos_Arr[d] + ")" + "</text>" 
+        }
+        else if (metaphorPos_Arr[d] == "Adjective")
+        {
+            metaphorPOS_color = "<text style='color:white; background-color: yellow; font-size:14px'>"+ "(" + metaphorPos_Arr[d] + ")" + "</text>" 
+        }
+        else if (metaphorPos_Arr[d] == "Adverb")
+        {
+            metaphorPOS_color = "<text style='color:white; background-color: green; font-size:14px'>"+ "(" + metaphorPos_Arr[d] + ")" + "</text>" 
+        }
         //highlight words
-        sentenceArr[d] = sentenceArr[d].replace(" "+wordArr[d], " "+"<text style='color:white; background-color: limegreen; font-size:14px'>"+""+wordArr[d]+"("+wordPos_Arr[d]+")"+"</text>");
-        sentenceArr[d] = sentenceArr[d].replace(" "+metaphorArr[d], " "+"<text style='color:white; background-color: orange; font-size:14px'>"+""+metaphorArr[d]+"("+metaphorPos_Arr[d]+")"+"</text>");
+        sentenceArr[d] = sentenceArr[d].replace(" "+wordArr[d], " "+"<text style='color:white; background-color: limegreen; font-size:14px'>"+wordArr[d]+"</text>"+" "+wordPOS_color);
+        sentenceArr[d] = sentenceArr[d].replace(" "+metaphorArr[d], " "+"<text style='color:white; background-color: orange; font-size:14px'>"+metaphorArr[d]+"</text>"+" "+metaphorPOS_color);
         console.log(sentenceArr[d]);
         newNode.innerHTML="<li> " + sentenceArr[d] + "</li>";
 
